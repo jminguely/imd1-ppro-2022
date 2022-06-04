7289f5f8de376b9a4cecdebc163bc231419e4111
deavetheferret <francois.devantay@studentfr.ch>
2022-06-04 20:50:01 +0200

    add the script and update the js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,11 @@ console.log('script started')
 
 function openMenu() {
     document.documentElement.classList.toggle('menu-open')
+
+}
+
+if (openMenu = true) {
+    console.log('function worked')
+} else {
+    console.log('function doesnt worked')
 }
