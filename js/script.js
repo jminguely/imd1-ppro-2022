8396ca783ba4b0f294324d6c4acb6e36f2a3995f
deavetheferret <francois.devantay@studentfr.ch>
2022-06-04 21:21:37 +0200

    update js file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 console.log('script started')
 
-<<<<<<< HEAD
 var functionIsRunning = false;
 
 function menuOpen() {
@@ -12,16 +11,4 @@ function menuOpen() {
         functionIsRunning = false;
         console.log('function stoped to work');
     }
-=======
-function openMenu() {
-    document.documentElement.classList.toggle('menu-open');
-}
-
-const works = true;
-
-if (works) {
-    console.log('function openMenu worked');
-} else {
-    console.log('function openMenu doesnt worked');
->>>>>>> main
 }
