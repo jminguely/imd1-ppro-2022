3f4aada0b065748119c0878e484e1c3d7861afa9
deavetheferret <francois.devantay@studentfr.ch>
2022-06-04 21:17:33 +0200

    update the js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 console.log('script started')
 
-function openMenu() {
-    document.documentElement.classList.toggle('menu-open')
+var functionIsRunning = false;
 
-}
-
-if (openMenu = true) {
-    console.log('function worked')
-} else {
-    console.log('function doesnt worked')
+function menuOpen() {
+    document.getElementById("viewport").classList.toggle('open');
+    if (!functionIsRunning) {
+        functionIsRunning = true;
+        console.log('function worked');
+    } else if (!functionIsRunning) {
+        functionIsRunning = false;
+        console.log('function stoped to work');
+    }
 }
