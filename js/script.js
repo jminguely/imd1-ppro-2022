ba27cebec327eea8040b6cabdc9b1322924516eb
deavetheferret <francois.devantay@studentfr.ch>
2022-06-08 14:58:27 +0200

    done the header and start the main

--- js/script.js
+++ js/script.js
@@ -12,3 +12,10 @@ function menuOpen() {
         console.log('function stoped to work');
     }
 }
+
+window.onload = function() {
+    var elements = document.getElementsByClassName("each-word");
+    for (var i=0; i<elements.length; i++) {
+      elements[i].innerHTML = elements[i].innerHTML.replace(/\\b([a-z])([a-z]+)?\\b/gim, "<span class='first-letter'>$1</span>$2");
+    }
+}
